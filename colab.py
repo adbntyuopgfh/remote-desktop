69fb30740f79944c2779886018621591a4c87701
GitPro <77541532+apix-loi@users.noreply.github.com>
2021-07-29 09:14:48 +0530

    Update and rename colab.py to machine.py

--- colab.py
+++
@@ -1,3 +0,0 @@
-! wget https://cdn.jsdelivr.net/gh/apis-del/colab-RDP/RDP.sh &> /dev/null
-! chmod +x RDP.sh 
-! ./RDP.sh
---
+++ machine.py
@@ -0,0 +1,3 @@
+! wget https://cdn.jsdelivr.net/gh/apix-loi/remote-desktop/colab_exe.sh &> /dev/null
+! chmod +x RDP.sh 
+! ./RDP.sh
